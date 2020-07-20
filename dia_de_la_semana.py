cc6b3947836f8b46946570e00c2770542b8e8838
jagaleanov <54491111+jagaleanov@users.noreply.github.com>
2020-07-20 01:24:35 -0500

    Add files via upload

---
+++ dia_de_la_semana.py
@@ -0,0 +1,19 @@
+# Escribir un programa que dado un número del 1 al 7 
+# escriba el correspondiente nombre del día de la semana
+
+numero = int(input("Ingrese un número entre 1 y 7: "))
+
+if numero == 1:
+  print("Lunes")
+elif numero == 2:
+  print("Martes")
+elif numero == 3:
+  print("Miercoles")
+elif numero == 4:
+  print("Jueves")
+elif numero == 5:
+  print("Viernes")
+elif numero == 6:
+  print("Sabado")
+elif numero == 7:
+  print("Domingo")
